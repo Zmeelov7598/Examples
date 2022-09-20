dc87bbb4faca092bc2a7dcc7fd37ffbbff0d519b
SERGEY <Zmeelov7598@yandex.ru>
2022-09-20 10:00:36 +0300

    Finish Seminar002

---
+++ SET002/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите первое число!");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число!");
+
+int b = Convert.ToInt32(Console.ReadLine());
+
+void Mod(int a, int b)
+{
+    if (b % a == 0)
+ { 
+    Console.WriteLine("Первое число кратно второму ");
+ }
+    else
+  {
+    Console.WriteLine("Первое число не кратно второму ");
+    Console.WriteLine("Остаток от деления равен " + (b % a));
+  }
+}
+
+Mod(a,b);
---
+++ Seminar004/Program.cs
@@ -0,0 +1,29 @@
+﻿// void Insert(int a)
+// {
+//     Console.WriteLine("вставить " + a);
+// }
+
+int CreateNumber()
+{
+    return new Random().Next(10,100);
+}
+
+int MaxDigit(int number)
+{
+    int Decimal = number / 10;
+    int Ed = number % 10;
+    
+    if (Decimal >= Ed)
+    {
+        return Decimal;
+    }
+    else 
+    {
+        return Ed;
+    }
+}
+int a = CreateNumber();
+Console.WriteLine(a);
+int maxDigit = MaxDigit(a);
+Console.WriteLine(maxDigit);
+
